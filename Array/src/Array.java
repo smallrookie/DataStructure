05bbff2e4d5188d77986da091f000aa757de5973
RookieFCB <1543666575@qq.com>
2019-11-13 21:54:56 +0800

    添加自定义数组输出修改功能

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -50,12 +50,44 @@ public class Array {
             throw new IllegalArgumentException("Add failed. Array is full.");
         }
         if (index < 0 || index > size) {
-            throw new IllegalArgumentException("Add failed. Required index >= 0 and index <= size");
+            throw new IllegalArgumentException("Add failed. Required index >= 0 and index <= size.");
         }
         for (int i = size - 1; i >= index; --i) {
             data[i + 1] = data[i];
         }
         data[index] = e;
         size++;
+    }
+
+    // 获取index索引位置的元素
+    int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Required index >= 0 and index <= size.");
+        }
+        return data[index];
+    }
+
+    // 修改index索引位置的元素
+    void set(int index, int e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Required index >= 0 and index <= size.");
+        }
+        data[index] = e;
+    }
+
+    // 打印输出Array数组
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d, capacity = %d\n", size, data.length));
+        res.append('[');
+        for (int i = 0; i < size; ++i) {
+            res.append(data[i]);
+            if (i != size - 1) {
+                res.append(", ");
+            }
+        }
+        res.append(']');
+        return res.toString();
     }
 }
--- Array/src/Main.java
+++ Array/src/Main.java
@@ -2,5 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Array arr = new Array(20);
+        for (int i = 0; i < 10; ++i) {
+            arr.addLast(i);
+        }
+        System.out.println(arr);
+
+        arr.add(1, 1000);
+        System.out.println(arr);
+
+        arr.addFirst(-1);
+        System.out.println(arr);
     }
 }
