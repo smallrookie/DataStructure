e485418396f22e819c3521655fed99ca4eec8c22
RookieFCB <smallrookie@yeah.net>
2019-11-14 17:33:37 +0800

    解决时间复杂度震荡问题

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -105,7 +105,8 @@ public class Array<E> {
         size--;
         data[size] = null; // loitering objects != memory leak
 
-        if (size == data.length / 2) {
+        // 解决时间复杂度震荡问题
+        if (size == data.length / 4 && data.length / 2 != 0) {
             resize(data.length / 2);
         }
 
