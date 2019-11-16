8a2815fb2b349034d66e76788ec02925a71b95be
RookieFCB <1543666575@qq.com>
2019-11-16 20:21:24 +0800

    添加虚拟头结点

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -24,11 +24,11 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyHead;  // 虚拟头结点
     private int size;
 
     public LinkedList() {
-        head = null;
+        dummyHead = new Node(null, null);
         size = 0;
     }
 
@@ -44,12 +44,7 @@ public class LinkedList<E> {
 
     // 在链表头添加新的元素e
     public void addFirst(E e) {
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-
-        head = new Node(e, head);
-        size++;
+        add(0, e);
     }
 
     // 在链表的index（0-based）位置添加新的元素e
@@ -57,20 +52,13 @@ public class LinkedList<E> {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Add failed. Illegal index.");
         }
-        if (index == 0) {
-            addFirst(e);
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
         }
-        else {
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++) {
-                prev = prev.next;
-            }
-//            Node node = new Node(e);
-//            node.next = prev.next;
-//            prev.next = node;
-            prev.next = new Node(e, prev.next);
-            size++;
-        }
+        prev.next = new Node(e, prev.next);
+        size++;
     }
 
     // 在链表末尾添加新的元素e
