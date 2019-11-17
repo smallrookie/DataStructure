161c500df5a7016f4e2d33cc2ccdad11c7ca62d3
RookieFCB <1543666575@qq.com>
2019-11-17 13:04:00 +0800

    使用链表实现栈

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,26 +1,56 @@
+import java.util.Random;
+
 public class Main {
+
+    // 测试使用stack运行opCount个push和pop操作所需时间，单位：秒
+    private static double testStack(Stack<Integer> stack, int opCount) {
+
+        long starTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - starTime) / 10e9 * 1.0;
+    }
 
     public static void main(String[] args) {
 
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int i = 0; i < 5; i++) {
-            linkedList.addLast(i);
-            System.out.println(linkedList);
-        }
+//        LinkedList<Integer> linkedList = new LinkedList<>();
+//        for (int i = 0; i < 5; i++) {
+//            linkedList.addLast(i);
+//            System.out.println(linkedList);
+//        }
+//
+//        linkedList.add(2, 666);
+//        System.out.println(linkedList);
+//
+//        linkedList.set(2, 999);
+//        System.out.println(linkedList);
+//
+//        linkedList.remove(2);
+//        System.out.println(linkedList);
+//
+//        linkedList.removeFirst();
+//        System.out.println(linkedList);
+//
+//        linkedList.removeLast();
+//        System.out.println(linkedList);
 
-        linkedList.add(2, 666);
-        System.out.println(linkedList);
+        int opCount = 10000000;
 
-        linkedList.set(2, 999);
-        System.out.println(linkedList);
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time_1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack Time: " + time_1 + "s");
 
-        linkedList.remove(2);
-        System.out.println(linkedList);
-
-        linkedList.removeFirst();
-        System.out.println(linkedList);
-
-        linkedList.removeLast();
-        System.out.println(linkedList);
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time_2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack Time: " + time_2 + "s");
     }
 }
+
