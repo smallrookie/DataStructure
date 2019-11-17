02f9cba7445ade069b9ae2091e151eba81786b21
RookieFCB <1543666575@qq.com>
2019-11-17 14:07:41 +0800

    使用链表实现队列

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -19,6 +19,23 @@ public class Main {
         return (endTime - starTime) / 10e9 * 1.0;
     }
 
+    // 测试使用q运行opCount个enqueue和dequeue操作所需时间，单位：秒
+    private static double testQueue(Queue<Integer> q, int opCount) {
+
+        long starTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - starTime) / 10e9 * 1.0;
+    }
+
     public static void main(String[] args) {
 
 //        LinkedList<Integer> linkedList = new LinkedList<>();
@@ -42,15 +59,27 @@ public class Main {
 //        linkedList.removeLast();
 //        System.out.println(linkedList);
 
-        int opCount = 10000000;
+        int opCount = 100000;
 
-        ArrayStack<Integer> arrayStack = new ArrayStack<>();
-        double time_1 = testStack(arrayStack, opCount);
-        System.out.println("ArrayStack Time: " + time_1 + "s");
+//        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+//        double time_1 = testStack(arrayStack, opCount);
+//        System.out.println("ArrayStack Time: " + time_1 + "s");
+//
+//        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+//        double time_2 = testStack(linkedListStack, opCount);
+//        System.out.println("LinkedListStack Time: " + time_2 + "s");
 
-        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
-        double time_2 = testStack(linkedListStack, opCount);
-        System.out.println("LinkedListStack Time: " + time_2 + "s");
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time_1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue Time: " + time_1 + "s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time_2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue Time: " + time_2 + "s");
+
+        LinkdedListQueue<Integer> linkdedListQueue = new LinkdedListQueue<>();
+        double time_3 = testQueue(linkdedListQueue, opCount);
+        System.out.println("linkdedListQueue Time: " + time_3 + "s");
     }
 }
 
