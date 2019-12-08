25b201b260e5d10fa798e26a154c0cba8143870f
smallrookie <1543666575@qq.com>
2019-12-08 21:09:38 +0800

    基于Linklisted实现Set集合

--- Set/src/LinkedListSet.java
+++ Set/src/LinkedListSet.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+public class LinkedListSet<E> implements Set<E> {
 
-public class LinkedListSet {
+    private LinkedList<E> list;
+
+    public LinkedListSet() {
+        list = new LinkedList<>();
+    }
+
+    @Override
+    public void add(E e) {
+        if (!list.contains(e))
+            list.addFirst(e);
+    }
+
+    @Override
+    public void remove(E e) {
+        list.removeElement(e);
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return list.contains(e);
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
 }
--- Set/src/Main.java
+++ Set/src/Main.java
@@ -2,30 +2,32 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        System.out.println("Pride and Prejudice");
-        ArrayList<String> words1 = new ArrayList<>();
-        if (FileOperation.readFile("pride-and-prejudice.txt", words1)) {
-            System.out.println("Total words: " + words1.size());
-
-            BSTSet<String> set1 = new BSTSet<>();
-            for (String word: words1)
-                set1.add(word);
-            System.out.println("Total different words: " + set1.getSize());
+    private static double testSet(Set<String> set, String filename) {
+        long startTime = System.nanoTime();
+        System.out.println(filename);
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile(filename, words)) {
+            System.out.println("Total words: " + words.size());
+            for (String word: words)
+                set.add(word);
+            System.out.println("Total different words: " + set.getSize());
         }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) * 10e-9 * 1.0;
+    }
+
+    public static void main(String[] args) {
+        String filename = "pride-and-prejudice.txt";
+
+        BSTSet<String> bstSet = new BSTSet<>();
+        double time1 = testSet(bstSet, filename);
+        System.out.println("BSTSet: " + time1 + "s");
 
         System.out.println();
 
-        System.out.println("A Tale of Two Cities");
-        ArrayList<String> words2 = new ArrayList<>();
-        if (FileOperation.readFile("a-tale-of-two-cities.txt", words2)) {
-            System.out.println("Total words: " + words2.size());
-
-            BSTSet<String> set2 = new BSTSet<>();
-            for (String word: words2)
-                set2.add(word);
-            System.out.println("Total different words: " + set2.getSize());
-        }
+        LinkedListSet<String> listSet = new LinkedListSet<>();
+        double time2 = testSet(listSet, filename);
+        System.out.println("LinkedListSet: " + time2 + "s");
     }
+
 }
