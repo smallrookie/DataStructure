4d17e2765659110263f2aa3059cded9b84f1102c
RookieFCB <1543666575@qq.com>
2019-12-10 21:55:10 +0800

    基于邻接表的图基本实现

--- Grapth/src/Main.java
+++ Grapth/src/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         AdjMatrix adjMatrix = new AdjMatrix("g.txt");
         System.out.println(adjMatrix);
+
+        AdjList adjList = new AdjList("g.txt");
+        System.out.println(adjList);
     }
 }
