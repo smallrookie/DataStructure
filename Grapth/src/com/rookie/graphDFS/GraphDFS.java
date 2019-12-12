53174b7e43c46b825c198a1009bfb9291379f270
RookieFCB <smallrookie@yeah.net>
2019-12-12 16:38:21 +0800

    返回图的连通分量个数

--- Grapth/src/com/rookie/graphDFS/GraphDFS.java
+++ Grapth/src/com/rookie/graphDFS/GraphDFS.java
@@ -11,13 +11,17 @@ public class GraphDFS {
     private ArrayList<Integer> pre = new ArrayList<>();
     // 图的后序深度优先遍历
     private ArrayList<Integer> post = new ArrayList<>();
+    // 图的连通分量个数
+    private int cccount = 0;
 
     public GraphDFS(Graph G) {
         this.G = G;
         visited = new boolean[G.V()];
         for (int v = 0; v < G.V(); v++) {
-            if (!visited[v])
+            if (!visited[v]) {
                 dfs(v);
+                cccount++;
+            }
         }
     }
 
@@ -43,6 +47,11 @@ public class GraphDFS {
         return post;
     }
 
+    // 返回图的连通分量个数
+    public int count() {
+        return cccount;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
 
         Graph g = new Graph("g.txt");
@@ -53,6 +62,8 @@ public class GraphDFS {
 
         System.out.print("post: ");
         System.out.println(graphDFS.post);
+
+        System.out.println(graphDFS.count());
     }
 
 }
